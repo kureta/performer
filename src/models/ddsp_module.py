525bdeff3b9264d443b03b23c2d32e603b5f672c
kureta <skureta@gmail.com>
2022-09-01 02:34:37 +0300

    this time fixed flipped ir

--- src/models/ddsp_module.py
+++ src/models/ddsp_module.py
@@ -122,7 +122,7 @@ class DDSP(LightningModule):
 
         if batch_nb == 0:
             # Save impulse-response as audio, once per epoch
-            ir = self.reverb.ir[:, 0].cpu().numpy().T
+            ir = np.flip(self.reverb.ir[:, 0].cpu().numpy().T, axis=-1)
             ir = wandb.Audio(ir, sample_rate=SAMPLE_RATE)
             audios = []
             amps = []
